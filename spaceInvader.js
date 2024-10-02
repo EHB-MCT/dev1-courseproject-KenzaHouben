969850e0927f20dbf78d43209274d6ef12c4f002
Kenza Houben <kenza.tabti@student.ehb.be>
2024-10-02 18:02:21 +0200

    spaceInvader klaar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,32 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+context.beginPath();
+context.rect(50,50,300,300);
+context.strokeStyle = "black";
+context.fillStyle = "black";
+context.fill();
+context.stroke();
+
+
+context.beginPath();
+context.rect(75,75,50,50);
+context.rect(275,75,50,50);
+context.rect(75,175,50,50);
+context.rect(75,225,50,50);
+context.rect(125,225,50,50);
+context.rect(175,225,50,50);
+context.rect(225,225,50,50);
+context.rect(175,275,50,50);
+context.rect(275,225,50,50);
+context.rect(275,175,50,50);
+
+
+
+context.strokeStyle = "pink";
+context.fillStyle = "pink";
+context.fill();
+
+context.stroke();
