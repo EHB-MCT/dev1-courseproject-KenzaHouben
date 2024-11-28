c719e072b1ecb26ebc5e000b351e0b550ad41bb8
Kenza Houben <kenza.tabti@student.ehb.be>
2024-11-28 14:06:06 +0100

    test

--- test/star.js
+++ test/star.js
@@ -4,16 +4,25 @@ import * as Utils from "../../scripts/utils.js";
 
 drawStar();
 
-function drawStar(x, y, hue) {
+function drawStar(x, y) {
     // context.fillStyle = Utils.hsl(hue, 50, 50);
     context.fillStyle = "yellow";
     context.beginPath();
-    context.moveTo(200, 200);
-    context.lineTo(200 + 50, 200 + 50);
-    context.lineTo(150 + 50, 250 + 50);
-    context.lineTo(200 - 50, 300 - 50);
+    context.moveTo(x, y);
+    context.lineTo(x + 50, y + 50);
+    context.lineTo(x + 50, y + 50);
+    context.lineTo(x - 50, y - 50);
     context.closePath();
     context.fill();
     context.stroke();
 }
 
+// context.fillStyle = "yellow";
+// context.beginPath();
+// context.moveTo(200, 200);
+// context.lineTo(200 + 50, 200 + 50);
+// context.lineTo(150 + 50, 250 + 50);
+// context.lineTo(200 - 50, 300 - 50);
+// context.closePath();
+// context.fill();
+// context.stroke();
