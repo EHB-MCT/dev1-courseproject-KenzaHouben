62e53775700bf534168bedaf08843f445031ef87
Kenza Houben <kenza.tabti@student.ehb.be>
2024-12-15 19:08:46 +0100

    TAAK KLAAR KENZA HOUBEN

--- exercises/miniSpaceInvader.js
+++ exercises/miniSpaceInvader.js
@@ -2,15 +2,15 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-signature();
+signature(1500, 900, 25);
 
-function signature() {
+function signature(xPosition, yPosition, size) {
     context.fillStyle = "pink";
     context.beginPath();
-    context.fillRect(1500, 900, 25, 25);
-    context.fillRect(1600, 900, 25, 25);
-    context.fillRect(1500, 950, 25, 25);
-    context.fillRect(1500, 975, 125, 25);
-    context.fillRect(1550, 1000, 25, 25);
-    context.fillRect(1600, 950, 25, 25);
+    context.fillRect(xPosition, yPosition, size, size);
+    context.fillRect(xPosition + 100, yPosition, size, size);
+    context.fillRect(xPosition, yPosition + 50, size, size);
+    context.fillRect(xPosition, yPosition + 75, size + 100, size);
+    context.fillRect(xPosition + 50, yPosition + 100, size, 25);
+    context.fillRect(xPosition + 100, yPosition + 50, size, size);
 }
